fbaf43f893a49baddf66d3b0c4e2689c8f98ab03
Ricky <ricmaranga@gmail.com>
2025-05-23 16:38:48 +0200

    Code cleaning

--- src/main/java/it/pose/trophies/buttons/ButtonCreator.java
+++ src/main/java/it/pose/trophies/buttons/ButtonCreator.java
@@ -2,29 +2,18 @@ package it.pose.trophies.buttons;
 
 import it.pose.trophies.Trophies;
 import org.bukkit.ChatColor;
-import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.enchantments.Enchantment;
-import org.bukkit.entity.Player;
-import org.bukkit.event.inventory.ClickType;
-import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.persistence.PersistentDataType;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Consumer;
 
 public class ButtonCreator {
-
-    private Material material;
-    private String name;
-    protected static Consumer<ButtonClickEvent> clickHandler;
-    private final List<String> lore = new ArrayList<>();
-    private static final Map<String, Consumer<ButtonClickContext>> clickHandlers = new HashMap<>();
-    private final int amount = 1;
-    public UUID buttonId;
 
     private ButtonCreator() {
     }
@@ -75,40 +64,9 @@ public class ButtonCreator {
             NamespacedKey key = new NamespacedKey(Trophies.getInstance(), "button-id");
             meta.getPersistentDataContainer().set(key, PersistentDataType.STRING, id);
             ButtonRegistry.register(id, handler);
-            
+
             return this;
         }
-    }
-
-    public ItemStack createItem() {
-        ItemStack item = new ItemStack(material, amount);
-        ItemMeta meta = item.getItemMeta();
-        meta.setDisplayName(name);
-        meta.setLore(lore);
-        meta.getPersistentDataContainer().set(new NamespacedKey(Trophies.getInstance(), "button-id"), PersistentDataType.STRING, buttonId.toString());
-        item.setItemMeta(meta);
-        return item;
-    }
-
-    public static class ButtonClickEvent {
-        public final Player player;
-        public final ClickType clickType;
-        public final Inventory inventory;
-
-        public ButtonClickEvent(Player player, ClickType clickType, Inventory inventory) {
-            this.player = player;
-            this.clickType = clickType;
-            this.inventory = inventory;
-        }
-    }
-
-    public static String getButtonId(ItemStack item) {
-        if (item == null || !item.hasItemMeta()) return null;
-
-        ItemMeta meta = item.getItemMeta();
-        NamespacedKey key = new NamespacedKey(Trophies.getInstance(), "button-id");
-
-        return meta.getPersistentDataContainer().get(key, PersistentDataType.STRING);
     }
 }
 
